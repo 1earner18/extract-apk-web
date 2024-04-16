64f21bb281cc998ba562c1af3564334327f7c66c
1earner18 <167148009+1earner18@users.noreply.github.com>
2024-04-16 07:45:24 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Extract APK Data",
+  title: "APP EXTRACTOR",
   description: "View AndroidManifest, resources, and assets from APK files",
 };
 
